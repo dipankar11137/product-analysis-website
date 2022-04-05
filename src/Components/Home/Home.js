3fd2054f90d24cf1116a387850a03fdcdc82ee41
Dipankar <dipankar15-11137@diu.edu.bd>
2022-04-05 21:32:59 +0600

    final

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,22 +11,24 @@ const Home = () => {
     console.log(products.slice(0, 3));
 
     return (
-        <div className='container mt-5 p-3'>
-            <div className='row '>
-                <div className='col-lg-6 col-12'>
-                    <div className='watch-details-style'>
-                        <h1>Your Next Watch</h1>
-                        <h2>Your Best Watch</h2>
-                        <p>A smartwatch is a wearable computer in the form of a watch; modern smartwatches provide a local touchscreen interface for daily use, while an associated smartphone app provides for management and telemetry</p>
-                        <button className='btn-style'>Live Demo</button>
+        <div>
+            <div className='container mt-5 p-3'>
+                <div className='row '>
+                    <div className='col-lg-6 col-12'>
+                        <div className='watch-details-style'>
+                            <h1>Your Next Watch</h1>
+                            <h2>Your Best Watch</h2>
+                            <p>A smartwatch is a wearable computer in the form of a watch; modern smartwatches provide a local touchscreen interface for daily use, while an associated smartphone app provides for management and telemetry</p>
+                            <button className='btn-style'>Live Demo</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-6 col-12 watch-style shadow-lg mb-3'>
+                        <img className='w-75' src={watch} alt="" />
                     </div>
                 </div>
-                <div className='col-lg-6 col-12 watch-style shadow-lg mb-3'>
-                    <img className='w-75' src={watch} alt="" />
-                </div>
+                <br />
+                < hr />
             </div>
-            <br />
-            < hr />
             <div className='mt-5'>
                 <h1 className='fw-bold'>Customer Reviews </h1>
                 <div className='products-container'>
@@ -40,8 +42,8 @@ const Home = () => {
                 </nav>
 
             </div>
-
         </div>
+
     );
 };
 
